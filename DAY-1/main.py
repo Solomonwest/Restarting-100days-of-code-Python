fc1f7bbb465f46d00c07d4c4e9a8ccde9de983b2
solomonwest <westngosolomon@gmail.com>
2026-04-12 03:04:55 +0100

    Let's Get Started

---
+++ DAY-1/main.py
@@ -0,0 +1,26 @@
+# Code Exercise 1: Printing Practice.
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+
+
+# Code Exercise 2: Debugging Practice.
+
+    # debug the code.
+# print(Notes from Day 1")
+#  print("The print statement is used to output strings.")
+# print("Strings are string of characters"
+# priint("String concatenation is done with the + sign")
+# print(("New Lines can be created with a \ and letter n")
+
+    # Output
+print("Notes from Day 1")
+print("The print statement is used to output strings.")
+print("Strings are string of characters")
+print("String concatenation is done with the + sign")
+print("New Lines can be created with a \ and letter n")
---
+++ DAY-1/task.py
@@ -0,0 +1,22 @@
+# Task 1 Printing
+print('Hello World!!')
+
+
+# Task 2 String Manipulation 
+
+# Adding a new line.
+print('Hello World!!\nHello World!!')
+
+# concatenation
+print('Hello' + ' ' + 'Solomon')
+
+
+# Task 3 Inputs
+
+print("Let's welcome the " + input("What is your Tech Stack? "))
+
+
+# Task 4 Variables 
+# 
+name = input("What's your Name?")
+print(len(name))
