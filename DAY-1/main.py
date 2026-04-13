d8b2fc706afd79fb92f6e95493f84408f7f8b3ac
solomonwest <westngosolomon@gmail.com>
2026-04-13 02:02:50 +0100

    Day 1 Finale

--- DAY-1/main.py
+++ DAY-1/main.py
@@ -24,3 +24,18 @@ print("The print statement is used to output strings.")
 print("Strings are string of characters")
 print("String concatenation is done with the + sign")
 print("New Lines can be created with a \ and letter n")
+
+
+# Code Exercise 3: Debugging Practice.
+
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. 
+# Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". 
+# You are only allowed to use variables to solve this exercise.
+
+# glass1 = "milk"
+# glass2 = "juice"
+
+# Answer
+glass1 = "milk"
+glass2 = "juice"
+glass1, glass2 = glass2, glass1    # Tuple Unpacking.
--- DAY-1/task.py
+++ DAY-1/task.py
@@ -1,22 +1,43 @@
 # Task 1 Printing
-print('Hello World!!')
 
+print('Hello World!!')
+# ------------------------------------------------------------------------------------------------------------
 
 # Task 2 String Manipulation 
 
 # Adding a new line.
+#  "\n is use to add a new line."
 print('Hello World!!\nHello World!!')
 
 # concatenation
 print('Hello' + ' ' + 'Solomon')
+# -------------------------------------------------------------------------------------------------------------
 
 
 # Task 3 Inputs
 
 print("Let's welcome the " + input("What is your Tech Stack? "))
+# -------------------------------------------------------------------------------------------------------------
 
 
 # Task 4 Variables 
-# 
+
 name = input("What's your Name?")
-print(len(name))
+print(len(name)) #len() is used to check the length of a string
+# -------------------------------------------------------------------------------------------------------------
+
+# Task 5 Variables Naming
+
+# 1. Make your code/naming readable.
+# 2. Use Underscore to seperate words.
+# 3. You can use numbers in naming, but don't start naming with a number.
+# 4. Don't make use of existing python keywords in naming, as they are unique and could cause conflict.
+# -------------------------------------------------------------------------------------------------------------
+
+# Task 6 Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+# -------------------------------------------------------------------------------------------------------------
