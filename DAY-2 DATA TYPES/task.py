ec6f481372ac2e5380d1e65cf0b7419877ae19dd
solomonwest <westngosolomon@gmail.com>
2026-04-14 03:58:37 +0100

    The start of a new day

---
+++ DAY-2 DATA TYPES/task.py
@@ -0,0 +1,29 @@
+# primitive data type
+# 1. strings
+# 2. integers
+# 3. float
+# 4. boolean
+
+#  To check the type of a data we use the type() keyword
+
+name = 'solomon'
+print(type(name)) # we get <class 'str'> as an output.
+
+# The method of pulling out a letter from a string is called Subscripting
+print("hello"[-1])
+
+# String 
+print("123"+"456")
+
+# Integer = whole numbers
+print(123 + 456)
+
+# Large Integers
+print(123_456_789) # we use the underscore(_) in representing large integers
+
+# Float = Floating Point Numbers
+print(3.141529)
+
+# Boolean
+print(True)
+print(False)
