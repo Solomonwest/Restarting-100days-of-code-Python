6b6d90a08667dcbd69ef1da264c58e1e2dd962c4
solomonwest <westngosolomon@gmail.com>
2026-05-03 20:12:35 +0100

    We just gained some momentum

--- DAY-4 RANDOMIZATION IN PYTHON/task.py
+++ DAY-4 RANDOMIZATION IN PYTHON/task.py
@@ -40,3 +40,39 @@ states_of_nigeria[1] = 'Osun'
 states_of_nigeria.append('Kogi')
 
 print(states_of_nigeria)
+
+
+# Task 3 Who will pay the Bill?
+
+friends = ['daniel', 'donald', 'malem', 'nicholas', 'david']
+
+total_friends = len(friends)
+
+rand_friend = random.randint(0, total_friends-1)
+
+# This prints out random friends from the list of friends.
+print(friends[rand_friend])
+
+
+
+# Task 4 Index Error
+
+# This error shows when the index of a list requested is out of range.
+#  The best way to handle that is to get the length of the list and subtract 1 from it.  e.g  
+
+num_of_states = len(states_of_nigeria)
+
+# with this we get the last data in the list
+print(states_of_nigeria[num_of_states-1])
+
+
+# Task 5  Nested List
+#  Having a List inside a list 
+vegetables = ['tomato', 'lettuce', 'cabbage', 'onions', 'onion']
+
+fruits = ['Apple', 'Guava', 'strawberry', 'pineaple']
+
+store = [vegetables, fruits]
+
+print(store)
+
