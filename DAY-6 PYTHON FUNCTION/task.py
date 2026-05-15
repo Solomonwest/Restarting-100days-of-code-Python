36218c687dde7299c0ff57d184ab5b66639fed34
solomonwest <westngosolomon@gmail.com>
2026-05-15 14:07:11 +0100

    Resume from where we stopped

---
+++ DAY-6 PYTHON FUNCTION/task.py
@@ -0,0 +1,12 @@
+# Task 1 -- Functon definition 
+
+# DEFINE A FUNCTON USING THE "DEF" KEYWORD
+# Note that all contents of a function are always indented in the function
+def my_func():
+    print("Hello")
+    print("BYE")
+
+# CALL A FUNCTION BY TYPING THE FUNCTION with braces '()'
+my_func()
+
+
