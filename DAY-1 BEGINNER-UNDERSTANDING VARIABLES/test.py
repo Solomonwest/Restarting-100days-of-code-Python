dfb878f53f1562705fa78658e7e95f92c1cca0b4
solomonwest <westngosolomon@gmail.com>
2026-07-09 18:08:27 +0100

    For leticia

---
+++ DAY-1 BEGINNER-UNDERSTANDING VARIABLES/test.py
@@ -0,0 +1,67 @@
+# name = "John"
+# age = '17'
+# student = True
+# weight = 70.5
+
+# names = ["John", "Jane", "Doe", [80, 12, 14]]
+
+# school = {
+#     'name': 'SOLOMON',
+#     'location': 'Lagos',
+#     'students': 500
+# }
+# unchange = (1,2,4,5,'heythere')
+
+
+# # print(name + age) 
+
+# # class EyeMaskerApp():
+# #     def 
+
+
+
+# # def print_name(name, age, level):
+# #     print(f"My name is {name}, i am {age} yrs old, i am a {level} level student.")
+
+# # user1 = print_name('leticia',20,100)
+# # print(name + ' ' + age)
+
+# print(10//2)
+
+
+
+
+num = [1,2,3,4,5,6,700]
+
+num1 = 55
+num2 = "45"
+
+int()
+str()
+num3 = float(num1)
+# num.extend([4,9,9])
+
+name = 'leticodes'
+
+name = 'solomoncodes'
+
+# print(str(num1) + ' ' + num2)
+
+name_list = list(name)
+
+# print(name)
+
+
+fruits = ["strawberry", "nectarines", "apples", "grapes", "peaches", "cherries", "pears"]
+vegetables = ["Spinach", "kale", "Tomatoes", "Celery", "Potatoes"]
+
+# fruits[-1] = 'Melons'
+# fruits.append("Lemons")
+
+detty_dozen = [fruits, vegetables]
+
+# print(fruits)
+
+
+a=2
+print(a)
