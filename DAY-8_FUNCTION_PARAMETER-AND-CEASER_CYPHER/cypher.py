bf2eeb64f05481d1fda63d551c8d0f989f6dfba3
solomonwest <westngosolomon@gmail.com>
2026-09-17 20:13:52 +0100

    Added ASCII art and decrypt function.

--- DAY-8_FUNCTION_PARAMETER-AND-CEASER_CYPHER/cypher.py
+++ DAY-8_FUNCTION_PARAMETER-AND-CEASER_CYPHER/cypher.py
@@ -1,5 +1,24 @@
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+print(r"""
+
+__        _______ _     ____ ___  __  __ _____ 
+\ \      / / ____| |   / ___/ _ \|  \/  | ____|
+ \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|  
+  \ V  V / | |___| |__| |__| |_| | |  | | |___ 
+   \_/\_/  |_____|_____\____\___/|_|  |_|_____|
+
+ _____ ___     ____    _    _____ ____    _    ____     ____ ___ ____  _   _ _____ ____  
+|_   _/ _ \   / ___|  / \  | ____/ ___|  / \  |  _ \   / ___|_ _|  _ \| | | | ____|  _ \ 
+  | || | | | | |     / _ \ |  _| \___ \ / _ \ | |_) | | |    | || |_) | |_| |  _| | |_) |
+  | || |_| | | |___ / ___ \|  |__ ___) / ___ \|  _ <  | |___ | ||  __/|  _  | |___|  _ < 
+  |_| \___/   \____/_/   \_\_____|____/_/   \_\_| \_\  \____|___|_|   |_| |_|_____|_| \_\
+      
+
+
+""")
+
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n").lower()
 
@@ -26,8 +45,8 @@ def encrypt(original_text, shift_amount):
 
     # ENCODE BY TAKING THE INDEX OF EVERY LETTER FROM THE ALPHABET LIST AND USE SAME INDEX TO FETCH FROM ENCRYPTED ALPHABET
     for i in original_text:
-        if i == ' ':
-            pass
+        if i not in alphabet:
+            encrypted_data.append(i)
         else:
             index = alphabet.index(i)
             encrypted_data.append(encrypted_alphabet[index])
@@ -35,10 +54,47 @@ def encrypt(original_text, shift_amount):
     # CONVERTING ENCRYPTED DATA INTO A STRING
     for letter in encrypted_data:
         encrypted_word += letter 
-        
-    print(f"your encrypted word is {encrypted_word}")
-    print(f"your encrypted data is {encrypted_data}")
 
+    print(f"your encrypted word is: {encrypted_word}")
     
-    
-encrypt(original_text=text, shift_amount=shift)
+
+
+decrypted_data = []
+def decrypt(original_text, shift_amount):
+
+
+    decrypted_word = ''
+    start = len(alphabet) - (shift_amount-1)
+
+    #  CREATE AN ENCRYPTED ALPHABET LIST BASED ON SHIFT AMOUNT
+    for i in range(1, shift_amount):
+        encrypted_alphabet.append(alphabet[start])
+        start +=1
+
+    for i in range(0, ((start + 1) - shift_amount)):
+        encrypted_alphabet.append(alphabet[i])
+
+    # DECODE BY TAKING THE INDEX OF EVERY LETTER FROM THE ENCYPTED ALPHABET LIST AND USE SAME INDEX TO FETCH FROM ALPHABET LIST
+    for i in original_text:
+        if i not in encrypted_alphabet:
+            decrypted_data.append(i)
+        else:
+            index = encrypted_alphabet.index(i)
+            decrypted_data.append(alphabet[index])
+
+    # CONVERTING ENCRYPTED DATA INTO A STRING
+    for letter in decrypted_data:
+        decrypted_word += letter 
+
+    print(f"your decrypted word is: {decrypted_word}")
+
+
+
+
+
+if direction == 'encode':
+    encrypt(original_text=text, shift_amount=shift)
+elif direction == 'decode':
+    decrypt(original_text=text, shift_amount=shift)
+else:
+    print("Invalid Command, Try Again")
